ce76f9e8d68a4d7b8e226de959880dc80a28a18a
fran2488 <frances.deboo@gmail.com>
2020-01-29 08:37:39 +0000

    emailjs set up

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -5,40 +5,40 @@ function userInformationHTML(user) {
                 (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
-    
-    <div class="gh-content">
-        <div class="gh-avatar">
-            <a href="${user.html_url}" target="_blank">
-                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
-            </a>
-        </div>
-        <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
-    </div>`;
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
 }
 
 function repoInformationHTML(repos) {
     if(repos.length == 0) {
         return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    let listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p> 
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>${listItemsHTML.join("\n")}</ul>            
+            </div>`;
 }
 
-let listItemsHTML = repos.map(function(repo) {
-    return `<li>
-                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-            </li>`
-});
-
-return `<div class="clearfix repo-list">
-            <p> 
-                <strong>Repo List:</strong>
-            </p>
-            <ul>${listItemsHTML.join("\n")}
-            </ul>            
-        </div>`;
-}
 
 function fetchGitHubInformation(event) {
     $("#gh-user-data").html("");
     $("#gh-repo-data").html("");
+
     let username = $("#gh-username").val();
     if(!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -59,13 +59,17 @@ function fetchGitHubInformation(event) {
                 let repoData = secondResponse[0];
 
                 $("#gh-user-data").html(userInformationHTML(userData));
-                $("#gh-user-data").html(repoInformationHTML(repoData));
+                $("#gh-repo-data").html(repoInformationHTML(repoData));
             }, 
             
             function(errorResponse) {
                 if (errorResponse.status === 404) {
                     $("#gh-user-data").html(
                         `<h2>No info found for user ${username}</h2>`);
+                } else if(errorResponse.status === 403) {
+                    let resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                    $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+
                 } else {
                     console.log(errorResponse);
                     $("#gh-user-data").html(
@@ -74,4 +78,4 @@ function fetchGitHubInformation(event) {
             });
 } 
 
-$(document).ready(fetchGitHubInformation());
+$(document).ready(fetchGitHubInformation);
